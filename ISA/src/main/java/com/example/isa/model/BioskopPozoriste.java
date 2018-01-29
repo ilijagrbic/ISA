074f4d2f81dc8edc7d241c1fd78035695a9298f7
ilijagrbic <ilijagrb.ns@gmail.com>
2018-01-29 17:07:44 +0100

    Samo ispomerao paket.

--- ISA/src/main/java/com/example/isa/model/BioskopPozoriste.java
+++ ISA/src/main/java/com/example/isa/model/BioskopPozoriste.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.isa.model;
 
 import javax.persistence.*;
 
--- ISA/src/main/java/com/example/isa/model/BioskopPozoristeType.java
+++ ISA/src/main/java/com/example/isa/model/BioskopPozoristeType.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.isa.model;
 
 public enum BioskopPozoristeType {
 	CINNEMA,
