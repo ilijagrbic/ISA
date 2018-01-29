da21cc4303b3bca7d7514503ac2d877b298798e7
ilijagrbic <ilijagrb.ns@gmail.com>
2018-01-29 17:05:48 +0100

    Dodao sam klasu za bioskop. Poceo model da se radja xD

---
+++ ISA/src/main/java/model/BioskopPozoriste.java
@@ -0,0 +1,74 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+public class BioskopPozoriste {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String name;
+	
+	private String description;
+	
+	private String address;
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private BioskopPozoristeType type;
+	
+	//@OneToOne
+	//Repertoar
+	
+	//@OneToMany
+	//Promotivne karte
+	
+	public BioskopPozoriste() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public BioskopPozoristeType getType() {
+		return type;
+	}
+
+	public void setType(BioskopPozoristeType type) {
+		this.type = type;
+	}
+	
+	
+	
+}
---
+++ ISA/src/main/java/model/BioskopPozoristeType.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum BioskopPozoristeType {
+	CINNEMA,
+	THEATRE
+}
