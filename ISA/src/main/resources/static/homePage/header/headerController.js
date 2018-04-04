54381698903f2b6cb85dad79cfb060e608d5d592
ilijagrbic <ilijagrb.ns@gmail.com>
2018-04-04 17:21:00 +0200

    Op op op, ovo radi?

--- ISA/src/main/resources/static/homePage/header/headerController.js
+++ ISA/src/main/resources/static/homePage/header/headerController.js
@@ -1,4 +1,15 @@
 angular.module('app')
-    .controller('headerController', function () {
-    	console.log("header controller calls")
+    .controller('headerController', function ($scope, $state) {
+    	if($state.$current=="defaultState"){
+    		$scope.headerState="home";
+    	}
+    	else if($state.$current=="cinemas"){
+    		$scope.headerState="cine";
+    	}
+    	else if($state.$current=="theatres"){
+    		$scope.headerState="thea";
+    	}
+    	$scope.headerSwitch=function(clicked){
+    		$scope.headerState=clicked;
+    	}
     });
