ec79c39bfa86e79901635ec7c876dafa78842007
Olja <andjelovskiolja@gmail.com>
2018-04-10 21:38:33 +0200

    Pocetak fronta - friendship

---
+++ ISA/src/main/resources/static/states/friendshipStates.js
@@ -0,0 +1,17 @@
+angular.module('app')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('friendship', {
+                url: '/friendship',
+                views: {
+                    'navbar': {
+                        templateUrl: 'homePage/header/header.html',
+                        controller: 'headerController'
+                    },
+                    'content': {
+                        templateUrl: 'friendship/friendship.html',
+                        controller: 'friendshipController'
+                    }
+                }
+            }
+    });
